3a08c2f1e113804f6c970d84d13a9ee04e2c7c78
developer <developer@LAPTOP-I16RDJ20.connect>
2019-02-04 21:19:18 +0000

    Updated Features and StepDefinition

--- AutomationLearning/src/test/java/features/Login.feature
+++ AutomationLearning/src/test/java/features/Login.feature
@@ -2,6 +2,7 @@ Feature: Appliation Login
 
 Scenario: Main Sign In Page
 Given User is on sign in page
-When User enters valid email and pasword
+When User enters valid email and password
 Then User is taken to fundraising pages
 And Sign in button is now My Account
+
---
+++ AutomationLearning/src/test/java/stepDefinition/stepDefinition.java
@@ -0,0 +1,30 @@
+package stepDefinition;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.And;
+
+
+public class stepDefinition {
+	@Given("^User is on sign in page$")
+	public void user_is_on_sign_in_page() 
+	{
+		
+	}
+	@When("^User enters valid email and password$")
+	public void user_enters_valid_email_and_password() 
+	{
+		
+	}
+	@Then("^User is taken to fundraising pages$")
+	public void user_is_taken_to_fundraiser_pages() 
+	{
+		
+	}
+	@And("^Sign in button is now My Account$")
+	public void Sign_in_button_is_now_My_Account() 
+	{
+		
+	}
+}
