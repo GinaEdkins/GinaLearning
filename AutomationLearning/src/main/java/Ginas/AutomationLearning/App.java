123b8c2f58d080438891efefaecb0a0f6b2f1cc2
developer <developer@LAPTOP-I16RDJ20.connect>
2019-02-04 20:34:33 +0000

    Initial Commit

---
+++ AutomationLearning/src/main/java/Ginas/AutomationLearning/App.java
@@ -0,0 +1,13 @@
+package Ginas.AutomationLearning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
