7784092e5ae4e98bc8dbe9e5ea57a79a7f645b79
Gina Edkins <g.edkins93@hotmail.com>
2019-02-05 10:39:24 +0000

    updated test runner and step definitions

--- AutomationLearning/src/test/java/stepDefinition/stepDefinition.java
+++ AutomationLearning/src/test/java/stepDefinition/stepDefinition.java
@@ -10,21 +10,22 @@ public class stepDefinition {
 	@Given("^User is on sign in page$")
 	public void user_is_on_sign_in_page() 
 	{
+		System.out.println("navigated to sign in page");
 		
 	}
 	@When("^User enters valid email and password$")
 	public void user_enters_valid_email_and_password() 
 	{
-		
+		System.out.println("user is signed in");
 	}
 	@Then("^User is taken to fundraising pages$")
 	public void user_is_taken_to_fundraiser_pages() 
 	{
-		
+		System.out.println("navigated to fundraiser pages");
 	}
 	@And("^Sign in button is now My Account$")
 	public void Sign_in_button_is_now_My_Account() 
 	{
-		
+		System.out.println("sign in button is now my account");
 	}
 }
